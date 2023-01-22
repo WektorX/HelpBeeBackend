486cf74ac898f00a2bc83b6c05b7a179e2e9c3e8
Wiktor Jurek <wiktor.jurek15@gmail.com>
2023-01-22 22:05:40 +0100

    Routes clean up

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,11 +1,9 @@
-import { Authentication } from './Authentication.js'
 import { Users } from './Users.js'
 import { Offers } from './Offers.js';
 import { Ratings } from './Ratings.js';
 import { Chat } from './Chat.js';
 
 export const Routes = {
-    listenAuth: Authentication,
     listenUsers: Users,
     listenOffers: Offers,
     listenRatings: Ratings,
